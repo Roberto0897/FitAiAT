06827a8e01be84f6b801d003e07de306586e9a9d
Roberto <antoniojunior11165@hotmail.com>
2025-11-27 22:13:19 -0300

    deploy at2

--- fitai_backend/fitai/__init__.py
+++
@@ -1,8 +0,0 @@
-
-import os
-
-# 🔥 Define qual settings usar ANTES de qualquer import do Django
-if os.environ.get('RENDER') or os.environ.get('DJANGO_SETTINGS_MODULE') == 'fitai.settings.production':
-    print("🚀 RENDER/PRODUCTION - Usando production.py")
-else:
-    print("🏠 LOCAL - Usando development.py")
